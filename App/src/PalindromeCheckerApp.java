d9149fb1c6d61b149078cb250937d9c6be151cb7
unknown <tonysharmila05@gmail.com>
2026-02-27 14:40:21 +0530

    welcome to Palindrome checker

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+    private static final String App_Name="Palindrome Checker App";
+    private static final String Version="1.0.0";
+
+    public static void main(String[] args){
+        System.out.println("===========================================");
+        System.out.println("           "+App_Name);
+        System.out.println("           version"+Version);
+        System.out.println("===========================================");
+        System.out.println("Welcome to palindrome chcker");
+        System.out.println("This Application Checks weather a given string is palindrome.");
+        System.out.println();
+        System.out.println(" Application Started Successfully:");
+
+    }
+}
