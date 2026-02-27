bb53daf6bc8dc9de0ca1a61d08c4fb36bc7b5b76
Jerry A <tonysharmila05@gmail.com>
2026-02-27 16:23:13 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,17 +3,17 @@ public class PalindromeCheckerApp {
 
         public static void main(String[] args) {
 
-            // Hardcoded string
+            
             String input = "madam";
 
             String reversed = "";
 
-            // Reverse string using loop
+            
             for (int i = input.length() - 1; i >= 0; i--) {
                 reversed = reversed + input.charAt(i);
             }
 
-            // Compare original and reversed
+           
             if (input.equals(reversed)) {
                 System.out.println("The given string is a Palindrome.");
             } else {
