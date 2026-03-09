f10ec9a588899bf17e410c888b16047423e5a348
jerry2919 <tonysharmila05@gmail.com>
2026-03-09 13:55:53 +0530

    uc8 palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,44 +1,44 @@
- import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
 
 public class PalindromeCheckerApp {
 
 
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase();
 
+        LinkedList<Character> list = new LinkedList<>();
 
-
-        public static void main(String[] args) {
-
-
-            String input = "refer";
-
-
-            Deque<Character> deque = new ArrayDeque<>();
-
-
-            for (char c : input.toCharArray()) {
-                deque.addLast(c);
-            }
-
-
-            boolean isPalindrome = true;
-
-
-            while (deque.size() > 1) {
-
-                char first = deque.removeFirst();
-                char last = deque.removeLast();
-
-                if (first != last) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-
-            System.out.println("Input : " + input);
-            System.out.println("Is Palindrome? : " + isPalindrome);
+        // Convert string to linked list
+        for (char c : input.toCharArray()) {
+            list.add(c);
         }
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        while (list.size() > 1) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
+}
